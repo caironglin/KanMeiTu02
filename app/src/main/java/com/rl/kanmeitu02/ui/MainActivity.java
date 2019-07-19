694182c7309fcf0de7d6c08ef3a54c88e09a0d38
RLWater <1226054531@qq.com>
2019-07-20 04:27:48 +0800

    Fix Material Design

--- app/src/main/java/com/rl/kanmeitu02/ui/MainActivity.java
+++ app/src/main/java/com/rl/kanmeitu02/ui/MainActivity.java
@@ -2,9 +2,18 @@ package com.rl.kanmeitu02.ui;
 
 import android.app.Activity;
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -20,7 +29,7 @@ import com.rl.kanmeitu02.utils.NetworkUtils;
 import java.util.ArrayList;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener ,NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = "MainActivity";
 
@@ -36,6 +45,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private SisterTask sisterTask;
     private SisterLoader mLoader;
     private SisterDBHelper mDbHelper;
+
+    private Toolbar toolbar;
+    private DrawerLayout drawer_layout;
+    private NavigationView nav_view;
+    private FloatingActionButton fab_github;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +69,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         nextBtn = (Button) findViewById(R.id.btn_next);
         showImg = (ImageView) findViewById(R.id.img_show);
 
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        nav_view = findViewById(R.id.nav_view);
+        nav_view.setItemIconTintList(null);
+        nav_view.setNavigationItemSelectedListener(this);
+
+        drawer_layout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.drawer_open, R.string.drawer_close);
+        drawer_layout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        fab_github = findViewById(R.id.fab_github);
+        fab_github.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view,getResources().getString(R.string.tip), Snackbar.LENGTH_SHORT).setAction("Action",null).show();
+            }
+        });
 
         previousBtn.setOnClickListener(this);
         nextBtn.setOnClickListener(this);
@@ -65,6 +99,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         sisterTask = new SisterTask();
         sisterTask.execute();
     }
+
+
 
     @Override
     public void onClick(View v) {
@@ -95,6 +131,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 break;
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_see_little_sister:
+                break;
+            case R.id.nav_use_check_weather:
+                break;
+            case R.id.nav_see_news:
+                break;
+            case R.id.nav_use_tools:
+                break;
+            case R.id.nav_else_setting:
+                break;
+            case R.id.nav_else_about:
+                break;
+        }
+        drawer_layout.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     private class SisterTask extends AsyncTask<Void,Void,ArrayList<Sister>> {
@@ -146,6 +202,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)){
+            drawer_layout.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
     }
 
     @Override
